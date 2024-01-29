7087fb26cf2ec012917e0dac23eca19db3099ebc
Ajay kumar <ajcoder25@gmail.com>
2024-01-29 07:03:21 +0530

    solved the question of if/else, let, var and const

---
+++ Javascript_fundamental-Part1/script.js
@@ -0,0 +1,55 @@
+
+
+
+// let country = "India"
+// let continent = "Asia"
+// let population = 400;
+
+
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+
+
+
+
+// //////////////////////////////
+
+// let isIsland = false;
+// let population = 400;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof language);
+
+
+
+// let firstName = "Ajay"
+
+
+// let currentYear = 2024;
+
+
+
+
+
+
+
+const checkBirth = 1998;
+
+let century;
+
+if (checkBirth <= 1995) {
+    century = 20;
+
+}
+else {
+    century = 21;
+
+}
+
+
+console.log(century);
