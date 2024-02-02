76a126b971c36bf3ff1999a2447ad073e0780042
Ajay kumar <ajcoder25@gmail.com>
2024-02-02 18:19:32 +0530

    Arrow Function

--- Javascript_fundamental-Part1/Javascript fundamental part2/script.js
+++ Javascript_fundamental-Part1/Javascript fundamental part2/script.js
@@ -75,3 +75,145 @@ const checkAge = function (birthYear) {
 
 const age2 = checkAge(1995);
 console.log(age2);
+
+
+
+// ///Question on function declaration vs Expression
+
+
+// function percentageOfWorld1(population) {
+//     return (population / 7900) * 100;
+// }
+
+// const percentageOfWorld2 = function (population) {
+//     return (population / 7900) * 100;
+// }
+
+
+///////////////////
+//Solving through function declaration
+
+// function percentageOfWorld1(population) {
+
+//     return (population / 100) * 100
+// }
+
+// const popOfChina = percentageOfWorld1(140);
+// const popOfIndia = percentageOfWorld1(141);
+// const popOfUsa = percentageOfWorld1(70);
+
+
+// console.log('Population China:' + 140 + '%')
+// console.log('Population India:' + 141 + '%')
+// console.log('Population of USA:' + 70 + '%')
+
+
+// //Solving it through function expression
+
+
+// const percentageOfWorld2 = function (population) {
+
+//     return (7900 / population) * 100
+// }
+
+
+// const checkChina = percentageOfWorld2(140);
+// const checkIndia = percentageOfWorld2(141)
+// const checkUsa = percentageOfWorld2(70)
+
+
+// console.log('China:' + checkChina + '%')
+// console.log('India:' + checkIndia + '%')
+// console.log('USA:' + checkUsa + '%')
+
+
+// Doing it agian
+
+//Function declaration
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100
+}
+
+
+const checkChina = percentageOfWorld1(140)
+const checkIndia = percentageOfWorld1(142)
+const checkUsa = percentageOfWorld1(50)
+
+
+
+
+console.log(`China's ${checkChina} 2nd Highest`)
+console.log(` India's ${checkIndia} highest`)
+console.log(`Usa's ${checkUsa} 3rd highest`)
+
+
+//function expression
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100
+}
+
+
+const nowChina = percentageOfWorld2(140);
+const nowIndia = percentageOfWorld2(145);
+const nowUsa = percentageOfWorld2(150)
+
+
+console.log('China' + nowChina)
+console.log('India' + nowIndia)
+console.log('nowUsa' + nowUsa)
+
+/////Arrow Function
+
+// const calcAge3 = birthYeah => 2037 - birthYeah
+// const age 3 = calcAge3(1991);
+
+
+
+
+// const checkCode = hoursCode => 24 - hoursCode
+// const howtoDefine = checkCode(5);
+// console.log(howtoDefine);
+
+
+
+///Arrow Function
+
+
+const yearsUntilRetirement = (birthyear, Name) => {
+    const age = 2037 - birthyear;
+    const retirement = 65 - age;
+    return `${Name} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, "Ajay"));
+
+
+const checkYourRetirement = (Name, years) => {
+
+    const age = 2037 - years
+    const retirmentAge = 65 - years
+    return `${Name} you are only left with these many ${retirmentAge} in service`
+
+}
+
+console.log(checkYourRetirement('Ajay', 1997))
+
+
+//Creating the Arrow Function
+
+
+const percentageOfWorld3 = (population) => {
+
+    return (population / 7900) * 100
+}
+
+
+const chinaPoP = percentageOfWorld1(1990);
+const indiaPop = percentageOfWorld3(1990)
+const UsaPop = percentageOfWorld3(1940)
+
+console.log('China:' + chinaPoP)
+console.log('India:' + indiaPop)
+console.log('UsaPop:' + UsaPop)
