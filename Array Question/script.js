ab1c20b992e479b9ed64647bf8a0517f049591ca
Ajay kumar <ajcoder25@gmail.com>
2024-02-13 06:11:40 +0530

    2nd day of solving array

--- Array Question/script.js
+++ Array Question/script.js
@@ -43,23 +43,63 @@ console.log(mixedDataType);
 //Q-6 
 const itcompanies = ['facebook', 'google', 'microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
 
-console.log(itcompanies);
-console.log(itcompanies.length);
+// console.log(itcompanies);
+// console.log(itcompanies.length);
 
-const firstCompany = itcompanies[0]
+// const firstCompany = itcompanies[0]
 
-console.log("first company:", firstCompany);
+// console.log("first company:", firstCompany);
 
-const middleCompany = itcompanies[(itcompanies.length - 1) / 2]
-console.log("middle Company:", middleCompany);
+// const middleCompany = itcompanies[(itcompanies.length - 1) / 2]
+// console.log("middle Company:", middleCompany);
 
-const lastCompany = itcompanies[itcompanies.length - 1]
-console.log('last company:', lastCompany);
+// const lastCompany = itcompanies[itcompanies.length - 1]
+// console.log('last company:', lastCompany);
 
 
-//printing the company
+// //printing the company
 
-console.log(itcompanies[0])
-console.log(itcompanies[1]);
-console.log(itcompanies[2]);
-console.log(itcompanies[3]);
+// console.log(itcompanies[0])
+// console.log(itcompanies[1]);
+// console.log(itcompanies[2]);
+// console.log(itcompanies[3]);
+
+
+/////////// to change in uppercase 
+
+// for (let i = 0; i < itcompanies.length; i++) {
+//     let changeToUppercase = itcompanies[i].toUpperCase()
+//     console.log(changeToUppercase);
+// }
+
+
+// //Q-12 
+// const sentence = itcompanies.slice(0, -1).join(', ') + ' and ' + itcompanies.slice(-1);
+// const finalSentance = sentence + ' are big IT companies'
+// console.log(finalSentance);
+
+
+/////// Q-13
+
+const value = prompt("check the comapany")
+let found = false;
+
+
+const lowerCaseValue = value.toLowerCase();
+
+for (let i = 0; i < itcompanies.length; i++) {
+    if (itcompanies[i].toLowerCase() === lowerCaseValue) {
+        found = true;
+
+        break;
+
+    }
+
+}
+if (found) {
+    console.log(`${value} this company is there`)
+}
+else {
+    console.log(`${value} not found `)
+}
+
