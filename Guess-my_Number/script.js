6acceea6a363dbad12f2b3d7946da917984f8e69
Ajay kumar <ajcoder25@gmail.com>
2024-02-17 07:09:33 +0530

    Guess my number

---
+++ Guess-my_Number/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+
+//change text using DOM Manipulation
+
+
+// document.querySelector('.message').textContent = '✔ correct Number!'
+
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+
+
+//handling of button
+
+document.querySelector('.check').addEventListener('click', function () {
+    console.log(document.querySelector('.guess').value)
+
+
+});
